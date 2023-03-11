bea14694cf0f809c2beb6444ab04210eacc975f6
shoaibakhterqadri <shoaiakhter181422@gmail.com>
2023-03-11 11:05:27 +0500

    static website is completed

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express=require('express');
-
 const app=express();
+const path=require('path');
 
+const staticPath=path.join(__dirname,'public');
+
+app.use(express.static(staticPath))
 app.get("/",(req,res)=>{
-      res.send("Hello world")
+      res.send("Heee")
 })
 
 app.get("/about",(req,res)=>{
       res.send("About Page")
 })
 
-app.listen(3000,()=>{
+app.listen(4000,()=>{
       console.log("Listing ")
 })
--- nav.js
+++ nav.js
@@ -1,23 +1,34 @@
-const express=require('express');
-const app=express();
-const port=3000;
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/',(req,res)=>{
-      res.send("Welcome to Home page");
-})
+app.get("/", (req, res) => {
+  res.write(" <h1> Welcome to Home page</h1>");
+  res.write(" <p> Welcome to Home page </p>");
+  res.send();
+});
 
-app.get('/about',(req,res)=>{
-      res.send("Welcome to About page");
-})
+app.get("/about", (req, res) => {
+  res.send("Welcome to About page");
+});
 
-app.get('/contact',(req,res)=>{
-      res.send("Welcome to Contact page");
-})
+app.get("/contact", (req, res) => {
+  res.send("Welcome to Contact page");
+});
 
-app.get('/temp',(req,res)=>{
-      res.send("Welcome to Temperature page");
-})
+app.get("/temp", (req, res) => {
+  res.json([
+    {
+      id: 45,
+      name: "Shoaib",
+    },
+    {
+      id: 45,
+      name: "Shoaib",
+    },
+  ]);
+});
 
-app.listen(port,()=>{
-      console.log(`Listening to the port on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Listening to the port on ${port}`);
+});
